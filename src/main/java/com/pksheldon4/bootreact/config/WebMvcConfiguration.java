d020bb590172473d0c2247fc02d519116376019a
Preston Sheldon <pksiv@hotmail.com>
2019-12-03 23:07:10 -0500

    Add postbuild to copy react-app/build to build/resources Add WebConviguration and Controller to serve resources from web folder.

---
+++ src/main/java/com/pksheldon4/bootreact/config/WebMvcConfiguration.java
@@ -0,0 +1,43 @@
+package com.pksheldon4.bootreact.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+@Slf4j
+@ConditionalOnProperty(prefix = "example", name = "react-ui-active", havingValue = "true")
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        log.info("##### addResourceHandlers()");
+        registry.addResourceHandler("/**/*")
+                .addResourceLocations("classpath:/web/")
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver() {
+                    @Override
+                    protected Resource getResource(String resourcePath, Resource location) throws IOException {
+                        log.info("resolving resource " + resourcePath);
+                        Resource requestedResource = location.createRelative(resourcePath);
+                        log.info("##### Looking for resource: " + requestedResource.toString());
+                        // Redirect anything that isn't registered as a route in the API to the react frontend router
+                        return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
+                                : new ClassPathResource("/web/index.html");
+                    }
+                });
+        registry.addResourceHandler("static/**/*")
+                .addResourceLocations("classpath:/web/static/")
+                .setCacheControl(CacheControl.maxAge(31536000, TimeUnit.SECONDS)) // 1 year
+                .resourceChain(true);
+    }
+}
---
+++ src/main/java/com/pksheldon4/bootreact/controller/HomeController.java
@@ -0,0 +1,40 @@
+package com.pksheldon4.bootreact.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+@ConditionalOnProperty(prefix = "example", name = "react-ui-active", havingValue = "true")
+public class HomeController implements ErrorController {
+    /**
+     * ꩜#%@*!꩜#%@*!꩜#%@*!꩜#%@*!꩜#%@*!꩜#%@*!꩜#%@*!꩜#%@*!꩜#%@*!꩜#%@*!
+     * ꩜#%@*!                                                     ꩜#%@*!
+     * ꩜#%@*!     Do not remove even though I look useless        ꩜#%@*!
+     * ꩜#%@*!                                                     ꩜#%@*!
+     * ꩜#%@*!꩜#%@*!꩜#%@*!꩜#%@*!꩜#%@*!꩜#%@*!꩜#%@*!꩜#%@*!꩜#%@*!꩜#%@*!
+     * <p>
+     * This is required to address a strange issue where the first time
+     * navigating to the base url results in a whitepage error
+     */
+    @GetMapping("/")
+    public String web() {
+        log.info("Calling HomeController with /");
+        return "index";
+    }
+
+//Note: If you use a path other than / you need to add this endpoint to ensure it contains the trailing /
+//    @GetMapping("/app")
+//    public RedirectView redirect(RedirectAttributes attributes) {
+//        log.info("Calling HomeController with /app");
+//        return new RedirectView("/app/");
+//    }
+
+    @Override
+    public String getErrorPath() {
+        return null;
+    }
+}
